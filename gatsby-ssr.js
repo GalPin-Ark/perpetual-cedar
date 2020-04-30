f7818b82ddaae6e91dc4de9887b423076f43fd4e
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-04-30 13:47:41 -0500

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,8 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        
+        <link href="https://admin.chatcompose.com/static/EN/global/export/css/main.0ec6acf6.css" rel="stylesheet"/>,
+        <script async type="text/javascript" src="https://admin.chatcompose.com/static/EN/global/export/js/main.dec259c7.js" user="enrique"></script>
     ]);
 
     setPostBodyComponents([
