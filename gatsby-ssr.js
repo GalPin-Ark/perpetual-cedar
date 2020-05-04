b1b69caf002899e14740906c458ddfcd77155ec7
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-05-04 10:16:59 -0500

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -10,7 +10,13 @@ const safePrefix = require("./src/utils/safePrefix").default;
 exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
 
     setHeadComponents([
-        
+        <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TPRH929');
+      `
+        }}
+      />
     ]);
 
     setPostBodyComponents([
@@ -19,7 +25,9 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={safePrefix('js/init.js')}/>
             <script src={safePrefix('js/main.js')}/>
             
-        </React.Fragment>
+        </React.Fragment>,
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPRH929"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     ]);
 
 };
