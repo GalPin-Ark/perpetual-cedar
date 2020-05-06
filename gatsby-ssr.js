4f3ecd954884ee764d883680c5481d2502ae02a6
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-05-06 13:42:36 -0500

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,20 +3,22 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
-const React = require("react");
-const safePrefix = require("./src/utils/safePrefix").default;
-
-exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
-
-    setHeadComponents([
-        <script
+/*
+ <script
         dangerouslySetInnerHTML={{
           __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TPRH929');
       `
         }}
       />
+*/
+const React = require("react");
+const safePrefix = require("./src/utils/safePrefix").default;
+
+exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
+
+    setHeadComponents([
+       
       
     ]);
 
