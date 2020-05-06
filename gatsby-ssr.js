1d06e3c904ba719fec6e95299fd417e65e157841
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-05-06 13:22:46 -0500

    Update gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -16,8 +16,7 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TPRH929');
       `
         }}
-      />,
-      <script type="text/javascript" id="hs-script-loader" async defer src="https://js.hs-scripts.com/7604870.js"></script>
+      />
       
     ]);
 
@@ -28,6 +27,8 @@ exports.onRenderBody = function({ setHeadComponents, setPostBodyComponents }) {
             <script src={safePrefix('js/main.js')}/>
             
         </React.Fragment>,
+        
+        <script type="text/javascript" id="hs-script-loader" async defer src="https://js.hs-scripts.com/7604870.js"></script>,
         <noscript dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPRH929"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
